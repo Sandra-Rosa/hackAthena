a9cbe18f9b1d95626f9e69fff8065e805b2da404
Sandra-Rosa <sandraantony2002@gmail.com>
2023-02-26 20:56:06 +0530

    added upto navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
+import coverimage from './assets/hackathena.jpg'
+import React, { useEffect, useState } from 'react';
 function App() {
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [screenHeight, setScreenHeight] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cover-container" style={{ width: `${screenWidth}px`, position: 'relative' }}>
+        <div className='rectangle' />
+        <nav className="navbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">ABOUT</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">INFO</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">TIMELINE</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">PRIZES</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">CONTACT</a>
+            </li>
+          </ul>
+        </nav>
+        <img
+          src={coverimage}
+          style={{ width: `${screenWidth}px`, objectFit: 'cover' }}
+        />
+      </div>
     </div>
   );
 }
